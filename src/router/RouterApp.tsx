382268975a0564b085467b14ae2528fe74f668ad
jesus <jesusmoralesvillegas@gmail.com>
2025-07-13 21:59:30 +0200

    pagina de clases

--- src/router/RouterApp.tsx
+++ src/router/RouterApp.tsx
@@ -1,12 +1,14 @@
 import { Navigate, Route, Routes } from "react-router";
 import { HomePage } from "../views/HomePage/HomePage";
-import { RazaPage } from "../views/RazasPage/Razapage";
+import { RazaPage } from "../views/RazasPage/RazaPage";
+import { ClasePage } from "../views/ClasesPage/ClasePage";
 
 export const RouterApp = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage></HomePage>}></Route>
       <Route path="/raza" element={<RazaPage></RazaPage>}></Route>
+      <Route path="/clase" element={<ClasePage></ClasePage>}></Route>
 
       <Route path="/*" element={<Navigate to="/"></Navigate>}></Route>
     </Routes>
